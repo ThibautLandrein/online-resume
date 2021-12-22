6d15a1e39589386e839971398ef24c09fb8ef078
ThibautLandrein <34551523+ThibautLandrein@users.noreply.github.com>
2021-12-22 13:56:11 +0100

    ajout des modèles (en cours) et context_processors

---
+++ resume/resume/context_processors.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 19 13:46:05 2021
+
+@author: Thibaut
+"""
+
+from django.contrib.auth.models import User
+
+
+def project_context(request):
+
+    context = {"me": User.objects.first()},
+
+    return context
