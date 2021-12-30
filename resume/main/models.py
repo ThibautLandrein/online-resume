cd019966059c2fdeaec9aa6cee7e11e32daed198
Thibaut Landrein <th.landrein@gmail.com>
2021-12-30 15:58:08 +0100

    Suppression boutons inutiles + ajout lien vers linkedin dans le model user profile

--- resume/main/models.py
+++ resume/main/models.py
@@ -33,6 +33,7 @@ class UserProfile(models.Model):
     bio = models.TextField(blank=True, null=True)
     skill = models.ManyToManyField(Skill, blank=True)
     cv = models.FileField(blank=True, null=True, upload_to="cv")
+    linkedin = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
